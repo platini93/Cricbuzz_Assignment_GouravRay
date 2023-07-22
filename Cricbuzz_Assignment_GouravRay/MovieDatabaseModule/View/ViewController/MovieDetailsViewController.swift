34164313be7888273175025a857b4f1ca312320d
platini93 <platinimaldini789@gmail.com>
2023-07-22 12:47:07 +0530

    Movie Details Ratings control added

--- Cricbuzz_Assignment_GouravRay/MovieDatabaseModule/View/ViewController/MovieDetailsViewController.swift
+++ Cricbuzz_Assignment_GouravRay/MovieDatabaseModule/View/ViewController/MovieDetailsViewController.swift
@@ -17,6 +17,12 @@ class MovieDetailsViewController: UIViewController {
     @IBOutlet weak var genreLbl: UILabel!
     @IBOutlet weak var plotTextView: UITextView!
     @IBOutlet weak var castAndCrewLbl: UILabel!
+    @IBOutlet weak var ratingsDropdown: DropDown!
+    
+    @IBOutlet weak var ratingValueOuterView: UIView!
+    @IBOutlet weak var ratingValueView: UIView!
+    @IBOutlet weak var ratingWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var ratingValueLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +33,60 @@ class MovieDetailsViewController: UIViewController {
         releaseDateLbl.text = "Released: \(movie.released)"
         genreLbl.text = "Genre: \(movie.genre)"
         plotTextView.text = "Plot: \(movie.plot)"
-        castAndCrewLbl.text = "Cast: \(movie.actors) \n\nDirectors: \(movie.director) \n\nWriters: \(movie.writer)"
+        castAndCrewLbl.text = "Cast: \(movie.actors) \nDirectors: \(movie.director) \nWriters: \(movie.writer)"
+        
+        var optionArray:[String] = []
+        var ratingValues:[String] = []
+        
+        for rating in movie.ratings {
+            optionArray.append(rating.source)
+            ratingValues.append(rating.value)
+        }
+        
+        ratingsDropdown.optionArray = optionArray
+        ratingsDropdown.isSearchEnable = false
+        ratingsDropdown.checkMarkEnabled = false
+        ratingsDropdown.selectedRowColor = UIColor.gray
+        ratingsDropdown.arrowColor = .black
+        ratingsDropdown.arrowSize = 15
+        
+        ratingsDropdown.text = "Select Source"
+        ratingValueLbl.text = ""
+        ratingWidthConstraint.constant = 0.0
+        
+        ratingsDropdown.didSelect{(selectedText , index ,id) in
+            self.ratingsDropdown.text = "\(selectedText)"
+            
+            let width = self.ratingValueOuterView.frame.size.width
+            let rawValue = ratingValues[index]
+            
+            if selectedText == "Internet Movie Database" {
+
+                let value = Double(rawValue.components(separatedBy: "/")[0])
+                let displayValue = (value ?? 0)/10.0 * Double(width)
+                self.ratingWidthConstraint.constant = CGFloat(displayValue)
+                self.ratingValueLbl.text = rawValue
+                
+            } else if selectedText == "Rotten Tomatoes" {
+                
+                let value = Double(rawValue.components(separatedBy: "%")[0])
+                let displayValue = (value ?? 0)/100.0 * Double(width)
+                self.ratingWidthConstraint.constant = CGFloat(displayValue)
+                self.ratingValueLbl.text = rawValue
+        
+            } else if selectedText == "Metacritic" {
+                
+                let value = Double(rawValue.components(separatedBy: "/")[0])
+                let displayValue = (value ?? 0)/100.0 * Double(width)
+                self.ratingWidthConstraint.constant = CGFloat(displayValue)
+                self.ratingValueLbl.text = rawValue
+                
+            }
+        }
+        
+        ratingValueOuterView.layer.cornerRadius = ratingValueOuterView.frame.size.height/2
+        ratingValueOuterView.clipsToBounds = true
+        
     }
     
 }
