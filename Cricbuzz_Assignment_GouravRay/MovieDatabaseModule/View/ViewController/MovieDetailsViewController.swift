4cdd85238a578bc73e8a42077789b3427447b381
platini93 <platinimaldini789@gmail.com>
2023-07-22 11:49:21 +0530

    movie details view added

--- Cricbuzz_Assignment_GouravRay/MovieDatabaseModule/View/ViewController/MovieDetailsViewController.swift
+++ Cricbuzz_Assignment_GouravRay/MovieDatabaseModule/View/ViewController/MovieDetailsViewController.swift
@@ -11,11 +11,23 @@ class MovieDetailsViewController: UIViewController {
     
     var movie:Movie = Movie()
     
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var releaseDateLbl: UILabel!
+    @IBOutlet weak var genreLbl: UILabel!
+    @IBOutlet weak var plotTextView: UITextView!
+    @IBOutlet weak var castAndCrewLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Movie Details"
-        
-        print("Movie : \(movie.title)")
+        plotTextView.flashScrollIndicators()
+        posterImage.sd_setImage(with: URL(string: movie.poster), placeholderImage: UIImage(named: "placeholder"))
+        titleLbl.text = movie.title
+        releaseDateLbl.text = "Released: \(movie.released)"
+        genreLbl.text = "Genre: \(movie.genre)"
+        plotTextView.text = "Plot: \(movie.plot)"
+        castAndCrewLbl.text = "Cast: \(movie.actors) \n\nDirectors: \(movie.director) \n\nWriters: \(movie.writer)"
     }
     
 }
